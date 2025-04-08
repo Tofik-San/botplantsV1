bee7aa8d81303ee9923a47413de12dfe13b89de7
Tofik-San <Tofkazlo@Gmail.com>
2025-04-08 22:22:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
 
+// Вставь сюда свой токен
+const token = 'ТВОЙ_ТОКЕН_СЮДА';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Привет! Я бот по растениям. Задай мне вопрос или выбери команду.');
+});
