cea07c9c94fa8d6751362a22263ed962d32bf543
Tofik-San <Tofkazlo@Gmail.com>
2025-04-10 18:30:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ bot.on('message', async (msg) => {
     const response = await openai.chat.completions.create({
       model: 'gpt-4',
       messages: [systemPrompt, ...recentHistory],
-      max_tokens: 400,
+      max_tokens: 1200,
     });
 
     const reply = response.choices[0].message.content;
