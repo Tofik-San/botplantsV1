38e238f9f495e9addd274f8f5aa5ce30f02861b5
Tofik-San <Tofkazlo@Gmail.com>
2025-04-10 20:36:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 3000;
-
+// Railway "держит" процесс только если есть сервер
 app.get('/', (req, res) => {
-  res.send('Ассистент Грин работает');
+  res.send('Бот активен');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Запускаем сам бот
+require('./bot');
